ca1c803a70e695b843e34c767e22ec30263efcba
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2020-06-19 20:02:21 -0400

    Cython implementation for zeta functions

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    name='Zeta Project',
     ext_modules=cythonize("zetaFunctions.pyx"),
-    zip_safe=False,
+    include_dirs=[numpy.get_include()]
 )
--- zetaFunctions.pyx
+++ zetaFunctions.pyx
@@ -1,54 +1,45 @@
+"""
+Name: Rajesh Rao
+Riemann Zeta Function
+Professor: Louis-Pierre Arguin
+Version 1.0
+--
+Cython programs for efficiently computing the various forms of the zeta function
+for use in research of the large value conjectures
+"""
+
 cimport numpy as nc
 
-from random import uniform
 from mpmath import zeta
-from numpy import zeros, array
-from math import log, sqrt, pi, cos
+from math import cos, sqrt, pi, log
+from random import uniform
 
 
-def zetaNormRange(int N , int deltaN) -> nc.ndarray:
+def zetaNorm(nc.complex128_t point) -> double:
     """
-    An itterative algorithm for computing the normalized range for the Zeta function
-    :param N: Provide a number (presumably large) to start computing values for
-    :param deltaN: Provide an integer number to cap off the consecutive sums of prime
-    :return: Returns an array of the normalized values for Zeta 
+    Computes the zeta norm at a given complex point
+    :param i: Provide a complex number to comute the zeta norm
+    :return: Returns a float, defined as the zeta norm
     """
-    cdef nc.complex128_t point = complex(0, 0)
-    cdef nc.complex128_t zeta_value = point
-    cdef int t = N
+    cdef double zetaN = abs(zeta(point))
 
-    # storage for the normal zeta 
-    cdef nc.ndarray zeta_array = zeros(shape=deltaN+1)
-    
-    # computing the Zeta function over a small interval defined by deltaN
-    for t in range(N, N+deltaN+1):
-        point = complex(real=0.5, imag=t)
-  
-        # computes the normalized zeta function -> refer to absoulte value code 
-        zeta_array[t-N] = abs(zeta(point))
-
-    return zeta_array
+    return zetaN
 
 
-def zetaUniformNorm(int T, int h) -> double:
+def logZetaNorm(nc.complex128_t i) -> double:
     """
-    An algorithm for computing the Zeta function with random uniform component
-    :param T: Provide a number (presumably large) to start computing values for
-    :param h: Provide an integer number to modify the imaginary component
-    :return: Returns an array of the normalized values for Zeta function with random uniform component
+    Computes the log zeta norm at a given complex point
+    :param i: Provide a complex number to comute the log zeta norm
+    :return: Returns a float, defined as the log zeta norm
     """
-    cdef nc.complex128_t point = complex(0, 0)
-    cdef double upper = 2 * T
+    cdef nc.complex128_t point = i
 
-    # computing the Zeta function for a single point h for a given T
-    point = zeta(complex(real=0.5, imag=uniform(T, upper) + h))
-
-    return abs(point)
+    return log(abs(zeta(point)))
 
 
 def zetaStochastic(double h, nc.ndarray prime_list) -> double:
     """
-    An itterative algorithm for computing the stochastic Zeta function defined X_t(h)
+    An iterative algorithm for computing the stochastic Zeta function defined X_t(h)
     :param h: Provide a floating interval range to observe the Zeta function
     :param prime list: Provide an array of prime numbers
     :return: Returns a value for the stochastic Zeta function 
@@ -57,8 +48,24 @@ def zetaStochastic(double h, nc.ndarray prime_list) -> double:
     cdef long i = 0
     cdef double upper = 2 * pi
     
-    # itterate through the primes using the specified model 
+    # iterate through the primes using the specified model
     for p in prime_list: 
         ret_val += ( 1/sqrt(p) * cos(uniform(0, upper) - h*log(p)) )
    
     return ret_val
+
+
+def zetaUniformNorm(int T, int h) -> double:
+    """
+    An algorithm for computing the Zeta function with random uniform component
+    :param T: Provide a number (presumably large) to start computing values for
+    :param h: Provide an integer number to modify the imaginary component
+    :return: Returns an value for the normalized values for Zeta function with random uniform component
+    """
+    cdef nc.complex128_t point = complex(0, 0)
+    cdef double upper = 2 * T
+
+    # computing the Zeta function for a single point h for a given T
+    point = zeta(complex(real=0.5, imag=uniform(T, upper) + h))
+
+    return abs(point)
